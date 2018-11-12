58a5e997997c8882496d379d4f700a53877a3cdb
Vanessa Hoffmann <hoffmann.vanessa13@gmail.com>
2018-11-12 13:20:18 -0600

    tried to make static images show u
    cried about it for a while
    
    did some stuff
    commented it out cause it didnt work
    aliza help me plz im not skilled in py
    aet up stuff for new pages to be made in the urls.py

--- MakingMagicHappen/rabbit/models.py
+++ MakingMagicHappen/rabbit/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+#from django.contrib.auth.models import User
+#from PIL import Image
 
 # Create your models here.
+#start of stuff to add images
+#class Catagory(models.Model):
+   # cname = models.CharField(max_length =200)
+  #  purdate = models.DateTimeField('purchase-date')
+ #   createdby = models.ForeignKey(User)
+
+#class Product (models.Mode):
+ #  pname = models.CharField(max_length=200)    
+ #  discription = models.CharField(max_length=500)
+ #  pirce = models.IntegerField()
+ #  noitem = models.IntegerField()
+ #   createdby = models.ForeignKey(User)
+ #   cid = models.ImageField(upload_to='photo')
+
+#class Cart(modles.Mode):
+#    uid = models.ForeignKey(User)
+#    pid = models.ForeignKey(Product)
+#    noitem = models.IntegerField()
+#   purdate = models.DateTimeField("purchase-date")
+#    deldate = models.DateTimeField("delivary-date")
+#end of stuff to add images
