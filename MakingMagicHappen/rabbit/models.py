1016afe4db059b8660012094aa941b9acffc81de
alliza <bistaaliza@gmail.com>
2018-11-14 00:50:12 -0600

    calendar_event model

--- MakingMagicHappen/rabbit/models.py
+++ MakingMagicHappen/rabbit/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+# model for the calendar event
+class calendarEvent(models.Model):
+    user= models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    event_name = models.CharField(max_length= 100, null= False)
+    event_time = models.TimeField((u"event Time"), auto_now_add=True, blank=True)
+    event_location = models.CharField(max_length = 1000, null= False)
+
