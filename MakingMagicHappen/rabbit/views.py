e3d2d3989967c303320abe9235b63cad8c70c0d3
Unknown <bsylve1996@gmail.com>
2018-11-19 17:56:16 -0600

    Form that are not working
    The form for creating and editing users for the superuser

--- MakingMagicHappen/rabbit/views.py
+++ MakingMagicHappen/rabbit/views.py
@@ -2,8 +2,8 @@ from django.shortcuts import render_to_response, render
 from django.views import generic
 from django.contrib.auth.models import User
 from django.views.generic import FormView, TemplateView
-from django.http import JsonResponse
-
+from django.http import JsonResponse, HttpResponseRedirect
+from .forms import modelUser
 
 # Create your views here.
 def home(request):
@@ -21,3 +21,16 @@ def validate_username(request):
         'is_taken' : User.objects.filter(username__isexact = username).e
     }
     return JsonResponse(data)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = modelUser(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/thanks')
+    else:
+        form = modelUser()
+    context = {
+        "form" : form
+    }
+    return render(request, 'rabbit/register.html', context)
