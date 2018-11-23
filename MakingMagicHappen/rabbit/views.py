2152378f7bc6ab8942da5038521ba416514c3aa5
alliza <bistaaliza@gmail.com>
2018-11-23 10:11:30 -0600

    small change in the form

--- MakingMagicHappen/rabbit/views.py
+++ MakingMagicHappen/rabbit/views.py
@@ -3,7 +3,6 @@ from django.views import generic
 from django.contrib.auth.models import User
 from django.views.generic import FormView, TemplateView
 from django.http import JsonResponse, HttpResponseRedirect
-from .forms import modelUser
 
 # Create your views here.
 def home(request):
@@ -15,22 +14,3 @@ def login(request):
 def calendar(request):
     return render(request, 'rabbit/calendar.html')
 
-def validate_username(request):
-    username = request.GET.get('username', None)
-    data = {
-        'is_taken' : User.objects.filter(username__isexact = username).e
-    }
-    return JsonResponse(data)
-
-
-def register(request):
-    if request.method == 'POST':
-        form = modelUser(request.POST)
-        if form.is_valid():
-            return HttpResponseRedirect('/thanks')
-    else:
-        form = modelUser()
-    context = {
-        "form" : form
-    }
-    return render(request, 'rabbit/register.html', {'form': form})
