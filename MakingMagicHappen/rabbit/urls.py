74ff205676afa8faf54013d5cdaac95001ddf4b7
alliza <bistaaliza@gmail.com>
2018-11-24 12:13:04 -0600

    added contact page and implemented googlemap api and emailservice in contact page

--- MakingMagicHappen/rabbit/urls.py
+++ MakingMagicHappen/rabbit/urls.py
@@ -3,6 +3,7 @@ from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
 
 from django.conf.urls import url
+from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.decorators import login_required
@@ -14,11 +15,13 @@ urlpatterns = [
     url(r'^about/$', TemplateView.as_view(template_name='rabbit/aboutUs.html'), name='about'),
     url(r'^login/$', auth_views.LoginView.as_view(template_name = 'rabbit/login.html'), name='login'),
     url(r'^adopt/$', TemplateView.as_view(template_name='rabbit/adopt.html'), name='adopt'),
-    url(r'^contact/$', TemplateView.as_view(template_name='rabbit/contact.html'), name='contact'),
+    # url(r'^contact/$', TemplateView.as_view(template_name='rabbit/contact.html'), name='contact'),
     url(r'^rabbits/$', TemplateView.as_view(template_name='rabbit/rabbits.html'), name='rabbits'),
+
     #url(r'^guinea-pigs/$', TemplateView.as_view(template_name='rabbit/guinea-pigs.html'), name='guinea-pigs'),
     #url(r'^care-info/$', TemplateView.as_view(template_name='rabbit/care-info.html'), name='care-info'),
     url(r'^calendar/$', TemplateView.as_view(template_name='rabbit/calendar.html'), name='calendar'),
     url(r'^register/$', CreateView.as_view(template_name='rabbit/register.html',form_class=UserCreationForm, success_url='/'),name='register'),
+    path('contact/', views.emailService, name='contact'),
 ]
 
