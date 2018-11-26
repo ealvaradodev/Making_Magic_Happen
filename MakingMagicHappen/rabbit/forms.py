6eb5dafb9619cd30a28fb630ab7999278675daf6
Myla Moore <myla.moore@ibm.com>
2018-11-26 16:30:19 -0600

    subimmission form

--- MakingMagicHappen/rabbit/forms.py
+++ MakingMagicHappen/rabbit/forms.py
@@ -1,5 +1,21 @@
 from django import forms
 from django.contrib.auth.models import User 
+
+class newUserForm(forms.Form): 
+    username = forms.CharField(max_length = 150, required = True) 
+    password = forms.CharField(max_length = 128, required = True,widget=forms.PasswordInput()) 
+    email = forms.EmailField(max_length = 254, required = True) 
+    first_name = forms.CharField(max_length = 30, required = False) 
+    last_name = forms.CharField(max_length = 150, required = False)
+    is_superuser = forms.BooleanField(required=False)
+
+class changingUserInfoForm(forms.Form): 
+    username = forms.CharField(max_length = 150, required = False) 
+    password = forms.CharField(max_length = 128, required = False,widget=forms.PasswordInput())
+    email = forms.EmailField(max_length = 254, required = False) 
+    first_name = forms.CharField(max_length = 30, required = False) 
+    last_name = forms.CharField(max_length = 150, required = False)
+    is_superuser = forms.BooleanField(required=False)
 #Enter Your Name
 #Enter email address
 #Enter your Subject
